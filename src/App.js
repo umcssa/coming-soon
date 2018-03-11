533a3ed38f4b9bc7e4055dc850f7df8cad8286c9
suwenhao <suwenhao@umich.edu>
2018-03-10 23:32:18 -0500

    add image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import construct from './images/constructing.gif';
 import './App.css';
 
+import RateMyProfessorNavbar from './RateMyProfessorNavbar';
+import RateMyProfessorNavbarCollapse from './RateMyProfessorNavbarCollapse';
+
+const centerParentStyle = {
+    display: 'table'
+};
+
+const centerChildStyle = {
+    display: 'table-cell',
+    verticalAlign: 'middle',
+    textAlign: 'center',
+};
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {window.innerWidth < 768 ? <RateMyProfessorNavbarCollapse/> : <RateMyProfessorNavbar/>}
+                <div style={{textAlign: 'center'}}>
+                    <img src={construct} style={{
+                        width: '100%',
+                    }}/>
+                    <p>Coming Soon</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
