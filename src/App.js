37331fa417d0a8fc1f99a1699f36db00b44d466d
kezian <kezian@umich.edu>
2018-03-11 01:08:36 -0500

    finish coming soon page

--- src/App.js
+++ src/App.js
@@ -20,18 +20,13 @@ class App extends Component {
         return (
             <div>
                 {window.innerWidth < 768 ? <RateMyProfessorNavbarCollapse/> : <RateMyProfessorNavbar/>}
-                <div style={{
-                    textAlign: 'center', height: window.innerWidth < 768 ? 600 : window.innerHeight,
-                    background: `url(${construct}) no-repeat center center`
-                }}>
-                    <div style={{position:'absolute',width:'100%'}}>
-                        <p style={{marginTop: 100, fontSize: window.innerWidth < 768 ? 50 : 100}}>敬请期待</p>
-                        <p style={{
-                            fontSize: window.innerWidth < 768 ? 40 : 80,
-                            marginTop: window.innerWidth < 768 ? -50 : -120
-                        }}>Coming Soon</p>
-                    </div>
-
+                <img src={construct} style={{height: window.innerHeight, minWidth: '100%', position: 'absolute', top: 0, right: 0}}/>
+                <div style={{position: 'absolute', width: '100%', top: 0, textAlign: 'center'}}>
+                    <p style={{marginTop: 100, fontSize: window.innerWidth < 768 ? 50 : 100}}>敬请期待</p>
+                    <p style={{
+                        fontSize: window.innerWidth < 768 ? 40 : 80,
+                        marginTop: window.innerWidth < 768 ? -50 : -120
+                    }}>Coming Soon</p>
                 </div>
             </div>
         );
