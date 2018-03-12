ed316076c62255122c751d3ea7d3815e7e67983a
kezian <kezian@umich.edu>
2018-03-11 23:11:13 -0400

    modify font size

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ class App extends Component {
     render() {
         return (
             <div>
-                {window.innerWidth < 768 ? <RateMyProfessorNavbarCollapse/> : <RateMyProfessorNavbar/>}
-                <img src={construct} style={{height: window.innerHeight, minWidth: '100%', position: 'absolute', top: 0, right: 0}} alt="Coming Soon"/>
+                {window.innerWidth < 992 ? <RateMyProfessorNavbarCollapse/> : <RateMyProfessorNavbar/>}
+                <img src={construct}
+                     style={{height: window.innerHeight, minWidth: '100%', position: 'absolute', top: 0, right: 0}}
+                     alt="Coming Soon"/>
                 <div style={{position: 'absolute', width: '100%', top: 0, textAlign: 'center'}}>
-                    <p style={{marginTop: 100, fontSize: window.innerWidth < 768 ? 50 : 100}}>敬请期待</p>
+                    <p style={{marginTop: 100, fontSize: Math.min(window.innerWidth / 10, 100)}}>敬请期待</p>
                     <p style={{
-                        fontSize: window.innerWidth < 768 ? 40 : 80,
-                        marginTop: window.innerWidth < 768 ? -50 : -120
+                        fontSize: Math.min(window.innerWidth / 10, 80),
+                        marginTop: -Math.min(window.innerWidth / 10, 120)
                     }}>Coming Soon</p>
                 </div>
             </div>
