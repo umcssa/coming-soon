1d0155e0815a6e4dcd1c627b07154e7b40a13b05
kezian <kezian@umich.edu>
2018-03-11 22:59:05 -0400

    change logo of navbar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,12 @@ import './App.css';
 import RateMyProfessorNavbar from './RateMyProfessorNavbar';
 import RateMyProfessorNavbarCollapse from './RateMyProfessorNavbarCollapse';
 
-const centerParentStyle = {
-    display: 'table'
-};
-
-const centerChildStyle = {
-    display: 'table-cell',
-    verticalAlign: 'middle',
-    textAlign: 'center',
-};
-
 class App extends Component {
     render() {
         return (
             <div>
                 {window.innerWidth < 768 ? <RateMyProfessorNavbarCollapse/> : <RateMyProfessorNavbar/>}
-                <img src={construct} style={{height: window.innerHeight, minWidth: '100%', position: 'absolute', top: 0, right: 0}}/>
+                <img src={construct} style={{height: window.innerHeight, minWidth: '100%', position: 'absolute', top: 0, right: 0}} alt="Coming Soon"/>
                 <div style={{position: 'absolute', width: '100%', top: 0, textAlign: 'center'}}>
                     <p style={{marginTop: 100, fontSize: window.innerWidth < 768 ? 50 : 100}}>敬请期待</p>
                     <p style={{
